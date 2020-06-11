b68c7120cb5e67a0948c4f81a763456fcf3bb9cb
Aximilli1212 <aximill1212@gmail.com>
2020-06-11 00:21:42 +0000

    update whistle blower api service

--- app/services/whistleblowerapi.js
+++ app/services/whistleblowerapi.js
@@ -1,4 +1,47 @@
-import Service from '@ember/service';
+// import Service from '@ember/service';
+//
+// export default Service.extend({
+// });
+//
 
-export default Service.extend({
+import Ember from 'ember';
+
+import axios from 'npm:axios';
+
+const ACCESS_TOKEN_KEY = 'access_token';
+
+const BASE_URL = 'http://localhost:3333';
+
+export default Ember.Service.extend({
+
+  getMeetups() {
+
+    const url = `${BASE_URL}/api/meetups`;
+
+    return axios.get(url, { headers: { Authorization: `Bearer ${this.getAccessToken()}` }}).then(response => response.data);
+
+  },
+
+  getWhistleBlowers() {
+
+    const url = `${BASE_URL}/api/whistleblowers`;
+
+    return axios.get(url, { headers: { Authorization: `Bearer ${this.getAccessToken()}` }}).then(response => response.data);
+
+  },
+
+  getActivities() {
+
+    const url = `${BASE_URL}/api/activities`;
+
+    return axios.get(url).then(response => response.data);
+
+  },
+
+  getAccessToken() {
+
+    return localStorage.getItem(ACCESS_TOKEN_KEY);
+
+  }
+
 });
