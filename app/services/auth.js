0799ac2d5a18fe96349431233ec93f86996b7ae2
Aximilli1212 <aximill1212@gmail.com>
2020-06-11 00:13:04 +0000

    import pacakages for auth service

--- app/services/auth.js
+++ app/services/auth.js
@@ -1,4 +1,10 @@
 import Service from '@ember/service';
+import decode from 'npm:jwt-decode';
+import auth0 from 'npm:auth0-js';
+const ID_TOKEN_KEY = 'id_token';
+const ACCESS_TOKEN_KEY = `access_token`;
 
 export default Service.extend({
+
+
 });
