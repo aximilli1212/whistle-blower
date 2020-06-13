65c7e461749de3670faef81858b234dcb4ffe411
Aximilli1212 <aximill1212@gmail.com>
2020-06-13 21:09:51 +0000

    add in base for meetups

--- app/routes/meetups.js
+++ app/routes/meetups.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import {inject} from '@ember/service';
 
 export default Route.extend({
+  api: inject.service('whistleblowerapi'),
+
+  model() {
+    return this.get('api').getMeetups();
+  }
 });
